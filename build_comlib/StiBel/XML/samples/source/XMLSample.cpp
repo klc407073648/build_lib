fe07e143f4e26f07a5988c1237f28994885418a3
klc407073648 <407073648@qq.com>
2023-06-07 06:23:34 +0800

    StiBel的samples内容合入

---
+++ build_comlib/StiBel/XML/samples/source/XMLSample.cpp
@@ -0,0 +1,16 @@
+#include "StiBel/XML/XmlUtil.h"
+#include <iostream>
+
+using namespace std;
+using namespace StiBel::XML;
+
+void fun()
+{
+    //TODO
+}
+
+int main(int argc, char **argv)
+{
+    fun();
+    return 0;
+}
---
+++ build_comlib/single_build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -x
+
+curPath=$(pwd)
+versionNum=''
+config_file='common.sh'
+cmake_build_type='Release'
+cmake_build_version='V2.1.1'
+
+[ ! -z $1 ] && cmake_build_type=$1
+
+[ ! -z $2 ] && cmake_build_version=$2
+
+function single_build() {
+	echo "single_build begin"
+
+	if [ ! -d ${curPath}/build ]; then
+		mkdir -p ${curPath}/build 
+	fi
+
+	rm -rf ${curPath}/build/*
+
+	cd ${curPath}/build
+
+	LIB_PATH=$curPath/../output/lib
+
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${LIB_PATH}/3partlib:${LIB_PATH}/comlib
+
+	cmake -DCMAKE_BUILD_TYPE=${cmake_build_type} -DCMAKE_BUILD_VERSION=${cmake_build_version} ..
+
+	make
+
+	echo "single_build end"
+}
+
+function MAIN() {
+	echo "[$FUNCNAME] ${0} begin"
+	single_build
+	echo "[$FUNCNAME] ${0} end"
+}
+
+MAIN
